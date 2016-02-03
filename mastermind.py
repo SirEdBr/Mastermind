d5099789e9c0b44e6020ecef1941a05ff6234103
Edward Bristow <SirEdBr@users.noreply.github.com>
2016-02-03 13:50:32 +0000

    Update #2
    Here is update #2! It includes the computer generating random numbers!

--- mastermind.py
+++ mastermind.py
@@ -1,4 +1,13 @@
+from random import random
+
 print("Welcome to Mastermind! This is a programme written in python 3.")
 input()
 print("If you do not know the rules to this game, look it up.")
 input()
+
+one = random.randint(1,6)
+two= random.randint(1,6)
+three = random.randint(1,6)
+four= random.randint(1,6)
+
+print("The computer has chosen four random numbers.")
