a9701983bc8bf975023eeacdb9353566730d6b37
Edward Bristow <SirEdBr@users.noreply.github.com>
2016-02-03 13:58:35 +0000

    Update #3
    Here is another update!!!
    
    :)

--- mastermind.py
+++ mastermind.py
@@ -11,3 +11,21 @@ three = random.randint(1,6)
 four= random.randint(1,6)
 
 print("The computer has chosen four random numbers.")
+print("Type the first number of your guess here:")
+1guess1 = input()
+print("Type the second number of your guess here:")
+1guess2 = input()
+print("Type the third number of your guess here:")
+1guess3= input()
+print("Type the fourth number of your guess here:")
+1guess4 = input()
+
+print()
+print("Here are your guess ratings.")
+
+if 1guess1 == one:
+  print(black)
+elif 1guess1 == two or 1guess1 == three or 1guess1 == four:
+  print(white)
+else:
+  print("Nothing")
